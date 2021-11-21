f445e650a003ef44b5edff9bb85a5e0b6f946778
Gustavo Araújo <gustavo.avk@msn.com>
2021-11-21 08:18:39 -0300

    fix: Correção Heroku

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 const express = require('express')
-const app = express()
-const server = require("http").createServer(app);
 const path = require("path")
 
 //Configurando porta no servidor Heroku, ou em local host
 const porta = process.env.PORT || 3000 
+
+const app = express()
+const server = require("http").createServer(app);
 
 app.use(express.static(path.join(__dirname,"public")))
 app.set('views',path.join(__dirname,"public"))
