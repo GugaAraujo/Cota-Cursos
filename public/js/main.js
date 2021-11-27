d4c8705ea89d6b7da79e43ecfbc6a1ae05ab8330
Gustavo Araújo <gustavo.avk@msn.com>
2021-11-27 15:53:14 -0300

    add:paginação

--- public/js/main.js
+++ public/js/main.js
@@ -15,7 +15,17 @@ let formatacao_monetaria = { minimumFractionDigits: 2 , style: 'currency', curre
 let lista_cursos = []
 
 // receberá temporariamente as informações da API, antes de as ordenarmos e parsearmos o JSON
-temporario = ""
+let temporario = ""
+
+//limite_por_pagina de itens por página
+let limite_por_pagina = 10
+
+//array qye abrigará todos as linhas de tabela, antes de serem quebradas em grupos de 10.
+let arraTr = []
+
+//UL botão de paginação, proximo e anterior
+let ul = document.querySelector(".lista_paginacao_ul")
+
 
 consulta_api()
 
@@ -23,47 +33,53 @@ consulta_api()
 
 // Consultando API
 function consulta_api(){
-    fetch(`https://testapi.io/api/Jonas-buriti/scholarships`)
+    fetch(`https://helper-guga.herokuapp.com`)
     //Aguardando Resultado
     .then(resultado => resultado.json())
     .then(resultado => recebe_json_API(resultado))
-    .catch(()=> exibe_erro("Não foi possível consultar a API. Tente novamente em algumas horas.","erro_alerta"))
-}
+    .catch((error)=> {
+      console.log(error)
+      exibe_erro("Não foi possível consultar a API. Tente novamente em algumas horas.","erro_alerta")
+    })
 
-//Alterando variáveis para facilitar a leitura...
-const recebe_json_API = (resultado)=>{
-    for (var contador=0; contador<resultado.length; contador++){
-        let preco_cheio = resultado[contador].full_price
-        let preco_desconto = resultado[contador].price_with_discount
-        let percentual_de_desconto = resultado[contador].discount_percentage
-        let campus_bairro = resultado[contador].campus.name
-        let campus_cidade = resultado[contador].campus.city
-        let curso_nome = resultado[contador].course.name
-        let curso_titulo = resultado[contador].course.kind
-        let curso_subtitulo = resultado[contador].course.level
-        let diponivel = resultado[contador].enabled
-        let inscricao = resultado[contador].enrollment_semester
-        let data_inicio = resultado[contador].start_date
-        let universidade = resultado[contador].university.name
-        let universidade_score = resultado[contador].university.score
-        let universidade_logo = resultado[contador].university.logo_url
 
-        //Filtrando objetos que pussuam Enable=True
-        if(diponivel){
-          //Adicionado ID para cada objeto
-            lista_cursos.push(`{"id":"${contador}","preco_cheio":${preco_cheio},"preco_desconto":${preco_desconto},"percentual_de_desconto":${percentual_de_desconto},"campus_bairro":"${campus_bairro}","campus_cidade":"${campus_cidade}","curso_nome":"${curso_nome}","curso_titulo":"${curso_titulo}","curso_subtitulo":"${curso_subtitulo}","diponivel":"${diponivel}","inscricao":"${inscricao}","data_inicio":"${data_inicio}","universidade":"${universidade}","universidade_score":"${universidade_score}","universidade_logo":"${universidade_logo}"}`)
+    //Alterando variáveis para facilitar a leitura...
+    const recebe_json_API = (resultado)=>{
+      for (var contador=0; contador<resultado.length; contador++){
+          let preco_cheio = resultado[contador].full_price
+          let preco_desconto = resultado[contador].price_with_discount
+          let percentual_de_desconto = resultado[contador].discount_percentage
+          let campus_bairro = resultado[contador].campus.name
+          let campus_cidade = resultado[contador].campus.city
+          let curso_nome = resultado[contador].course.name
+          let curso_titulo = resultado[contador].course.kind
+          let curso_subtitulo = resultado[contador].course.level
+          let diponivel = resultado[contador].enabled
+          let inscricao = resultado[contador].enrollment_semester
+          let data_inicio = resultado[contador].start_date
+          let universidade = resultado[contador].university.name
+          let universidade_score = resultado[contador].university.score
+          let universidade_logo = resultado[contador].university.logo_url
 
-          }
+          //Filtrando objetos que pussuam Enable=True
+          if(diponivel){
+            //Adicionado ID para cada objeto
+              lista_cursos.push(`{"id":"${contador}","preco_cheio":${preco_cheio},"preco_desconto":${preco_desconto},"percentual_de_desconto":${percentual_de_desconto},"campus_bairro":"${campus_bairro}","campus_cidade":"${campus_cidade}","curso_nome":"${curso_nome}","curso_titulo":"${curso_titulo}","curso_subtitulo":"${curso_subtitulo}","diponivel":"${diponivel}","inscricao":"${inscricao}","data_inicio":"${data_inicio}","universidade":"${universidade}","universidade_score":"${universidade_score}","universidade_logo":"${universidade_logo}"}`)
+
+            }
+
+      }
+
+      //parseando em JSON
+      temporario = JSON.parse(`[${lista_cursos}]`)
+
+      //ordenando array de cursos de acordo com a escolha do usuário
+      ordena_cursos("alfabetica")
 
     }
-
-    //parseando em JSON
-    temporario = JSON.parse(`[${lista_cursos}]`)
-  
-    //ordenando array de cursos de acordo com a escolha do usuário
-    ordena_cursos("alfabetica")
-
 }
+
+
 
 // Fechar modal
 window.onclick = function(event) {
@@ -162,21 +178,28 @@ function ordena_cursos(ordem){
 
   //renderizando após ordenado
   rendiza_tabela()
+
+  //Paginação de 10 em 10
+  //limita_itens_paginação(limite_por_pagina)
+
+  btn_gerador(limite_por_pagina)
 }
 
 //criando elementos da tabela
 function rendiza_tabela(){
   div_conteudo.innerHTML=""
   var tabela = document.createElement("table");
-  var tblBody = document.createElement("tbody");
+  var tbody = document.createElement("tbody");
+  tbody.className= "list-group"
 
   // criando as celulas
-  for (var contador=0; contador<lista_cursos.length;contador++) {
+  for (var contador=0; contador<limite_por_pagina;contador++) {
     // criando as linhas e inserindo dinamicamente cada elemento do json
     var row = document.createElement("tr")
-    row.className = "linha" 
+    row.className = "list-group-item linha" 
 
     row.id = `tr_${lista_cursos[contador].id}`
+
     //Coluna com imagem recebeu também atributo Title, para melhorar acessibilidade.
     row.innerHTML=`<td class="td_logo"><img class="img_logo"
     title="Universidade ${lista_cursos[contador].universidade}. ${lista_cursos[contador].curso_nome} - ${lista_cursos[contador].campus_cidade}. Valor: ${(lista_cursos[contador].preco_cheio).toLocaleString('pt-BR', formatacao_monetaria)}" 
@@ -190,15 +213,15 @@ function rendiza_tabela(){
     <i class="fas fa-plus-circle"></i>
     </a></td>`
 
-  // add as linhas em tblBody
-    tblBody.appendChild(row);
+  // add as linhas em tbody
+    tbody.appendChild(row);  
   }
 
   // inserindo <tbody> na <table>
-  tabela.appendChild(tblBody);
+  tabela.appendChild(tbody);
   // inserindo <table> em <section> div_conteudo
   div_conteudo.appendChild(tabela);
-  }
+}
 
 //exibindo erro ao usuário, informando a mensagem a ser exiba e também a classe (css) que receberá.
 function exibe_erro(string_erro,classe_erro){
@@ -214,7 +237,135 @@ function exibe_erro(string_erro,classe_erro){
 
 
 
- 
+//Paginação de 10 em 10
+// function limita_itens_paginação(limite_por_pagina){
+
+
+//   //pegando os elementos da tabela renderizada
+//   let tbody = document.getElementsByClassName("list-group")[0]
+//   let tr = document.getElementsByClassName("list-group-item linha")
+
+//   //contador, inserindo linha por linha na array de Tr's
+//   for(var i=0; i<tr.length;i++){
+//     arraTr.push(tr[i])
+//   }
+  
+//   //zerando a tabela, para não sobrepor dados
+//   tbody.innerHTML = ""
+
+//   //adiconando a tabela até atingir o limite_por_pagina estabelecido
+//   for(var i=0; i<limite_por_pagina;i++){
+//     tbody.appendChild(arraTr[i])
+//   }
+
+// }
+
+
+  function btn_gerador(limite_por_pagina)
+  {
+    ul.innerHTML=
+    `<li class="proximo d-inline-flex"> 
+    <a href="#" id="anterior">&#139;</a>
+  </li>
+  <li class="anterior d-inline-flex">
+    <a href="#" id="proximo">&#155;</a>
+  </li>`
+
+    let tbody = document.getElementsByClassName("list-group")[0]
+    //totald de itens da array
+    const numero_Tr = lista_cursos.length
+    
+    if(numero_Tr <= limite_por_pagina){
+      ul.style.display = "none"
+    }
+    else{
+      ul.style.display = "flex"
+      const divisao = Math.ceil(numero_Tr/limite_por_pagina)
+
+
+      for(let contador=1; contador<=divisao;contador++){
+        let li = document.createElement("li")
+        li.className = "lista_pagina d-inline-flex"
+        let a = document.createElement("a")
+        a.href = "#"
+        a.setAttribute("data-page",contador)
+        li.appendChild(a)
+        a.innerHTML = contador;
+        ul.insertBefore(li,ul.querySelector(".anterior"))
+
+        
+        a.onclick = e=>{
+          //número da página clicada
+          let x = e.target.getAttribute("data-page")
+          //limpando a tabela
+          tbody.innerHTML = ""
+          //reduzindo um, para bater com número da índice
+          x--
+
+          //defininfo em qual indice começaremos a "cortar" o array de TRs
+          let inicio_trecho = limite_por_pagina * x
+          //defininfo em qual indice terminaremos de "cortar" o array de TRs
+          let final_trecho = inicio_trecho + limite_por_pagina
+          //"cortando" o array de TRs para exibir somente este trecho na página clicada
+          let pagina = lista_cursos.slice(inicio_trecho,final_trecho)
+
+
+
+          //adicionando cada item do trecho no corpo da tabela
+          for(let contador=0; contador<pagina.length; contador++){
+            let item = pagina[contador]
+            // criando as linhas e inserindo dinamicamente cada elemento do json
+            var row = document.createElement("tr")
+            row.className = "list-group-item linha" 
+
+            row.id = `tr_${item.id}`
+
+            //Coluna com imagem recebeu também atributo Title, para melhorar acessibilidade.
+            row.innerHTML=`<td class="td_logo"><img class="img_logo"
+            title="Universidade ${item.universidade}. ${item.curso_nome} - ${item.campus_cidade}. Valor: ${(item.preco_cheio).toLocaleString('pt-BR', formatacao_monetaria)}" 
+            src="${item.universidade_logo}"/></td>`
+
+            row.innerHTML+=`<td class="td_curso" name="td_curso">${item.curso_nome}</td>`
+            row.innerHTML+=`<td class="td_cidade" name="td_cidade">${item.campus_cidade}</td>`
+            row.innerHTML+=`<td class="td_preco_cheio">${(item.preco_cheio).toLocaleString('pt-BR', formatacao_monetaria)}</td>`
+            row.innerHTML+=`<td class="td_link"><a 
+            href="#" class="link_tabela" id="${item.id}" onclick="abrir_modal(${contador})">
+            <i class="fas fa-plus-circle"></i>
+            </a></td>`
+
+            tbody.appendChild(row)
+          }
+        }
+      }
+
+
+      
+    }
+
+    let z = 0;
+    function btn_proximo(){
+
+      if(this.id=="proximo"&&z<=limite_por_pagina){
+        z == arraTr.length - limite_por_pagina ? (z=0) : (z+= limite_por_pagina)      
+      }
+      if(this.id=="anterior"){
+        z == 0 ? arraTr.length - limite_por_pagina : (z -= limite_por_pagina)
+      } 
+
+      tbody.innerHTML = "";
+      for(let contador=z; contador<z+limite_por_pagina; contador++){
+        tbody.appendChild(arraTr[contador])
+      }
+    }
+
+    document.getElementById("anterior").onclick = btn_proximo
+    document.getElementById("proximo").onclick = btn_proximo
+
+  }
+  
+  
+  
+
 
 
 
